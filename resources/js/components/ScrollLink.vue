90479f79dbe84c3297704d1e6e0ee8dc3df46592
Jack Ford <jack-ford@outlook.com>
2020-05-01 23:46:23 +0100

    Impletment ScrollTo functionality
    Require in-viewport package.
    Create Visible and ScrollTo Vue Components
    Create ScrollTo Setting in CP and implement on layout.antlers.html

---
+++ resources/js/components/ScrollLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <button :href="href" @click.prevent="scroll">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        props: ['href'],
+        methods: {
+            scroll() {
+                // For older browsers, consider pulling in a polyfill.
+                // https://www.npmjs.com/package/smoothscroll-polyfill
+                document.querySelector(this.href)
+                        .scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    }
+</script>
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,4 +1,6 @@
 import SlideoutMenu from './components/SlideoutMenu.vue';
+import ScrollLink from './Components/ScrollLink';
+import Visible from './Components/Visible';
 
 /**
  * First we will load all of this project's JavaScript dependencies.
@@ -11,7 +13,8 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('slideout-menu', SlideoutMenu);
-
+Vue.component('scroll-link', ScrollLink);
+Vue.component('visible', Visible);
 
 const site = new Vue({
     el: '#site',
